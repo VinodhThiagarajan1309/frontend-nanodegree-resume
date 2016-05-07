b13076f168c2bf384fdea87ff25a5a11aa618759
Vinodh Thiagarajan <vinodhthiagarajan@Vinodhs-MacBook-Pro.local>
2016-05-07 00:37:51 -0500

    Completed changes , next styling

--- js/helper.js
+++ js/helper.js
@@ -26,7 +26,7 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
@@ -133,9 +133,9 @@ function initializeMap() {
     // the locations array. Note that forEach is used for array iteration
     // as described in the Udacity FEND Style Guide: 
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    /*education.schools.forEach(function(school){
       locations.push(school.location);
-    });
+    });*/
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
@@ -239,11 +239,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -34,7 +34,6 @@ headerId.prepend(HTMLheaderName.replace('%data%',bio.name));
 
 /*Contacts*/
 topContactId.append(HTMLcontactGeneric.replace('%data%',bio.contacts.mobile));
-topContactId.append(HTMLmobile.replace('%data%',bio.contacts.mobile));
 topContactId.append(HTMLemail.replace('%data%',bio.contacts.email));
 topContactId.append(HTMLgithub.replace('%data%',bio.contacts.github));
 topContactId.append(HTMLlocation.replace('%data%',bio.contacts.location));
@@ -45,32 +44,173 @@ headerId.append(HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage));
 header
 
 /*Skills and Skill List*/
-var skillsId = $('#skills');
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
-
 headerId.append(HTMLskillsStart);
 
 for(i in bio.skills){
-	skillsId.append(HTMLskills.bio.skills);
+	$('#skills').append(HTMLskills.replace('%data%',bio.skills[i]));
 }
 
-
-/*Work Experience*/
+/*Work Json*/
 var work = {
-  "email": [
-	"Java Developer",
-	"Technology Analyst",
-	"Senior Software Engineer",
-	"Software Engineer",
-	"Software Trainee"
-  ],
-  "employer": "hackcorp llc",
-  "title": "Java Developer",
-  "location":"Austin,Texas",
-  "dates": "in progress",
-  "descrition": "Developing Insurance Application",
+  "jobs": [{
+    "employer": "Company 2",
+    "title": "Java Developer",
+    "location": "Austin,Texas",
+    "dates": "in progress",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  }, {
+    "employer": "Company 1",
+    "title": "Java Developer",
+    "location": "Fortworth,Texas",
+    "dates": "in progress",
+    "description": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+  }, {
+    "employer": "Company 1",
+    "title": "Java Developer",
+    "location": "Austin,Texas",
+    "dates": "Chennai,Tamilnadu",
+    "description": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+  }, {
+    "employer": "Company 1",
+    "title": "Java Developer",
+    "location": "Chennai,Tamilnadu",
+    "dates": "in progress",
+    "description": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+  }, {
+    "employer": "Company 1",
+    "title": "Java Developer",
+    "location": "Mysore,India",
+    "dates": "in progress",
+    "description": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+  }],
   "display": "function(){}"
 }
 
+//Get the workExperience id
 var workExperience = $('#workExperience');
+
+//Iterate the jobs json
+for(i in work.jobs){
+workExperience.append(HTMLworkStart);
+workExperience.append(HTMLworkEmployer.replace('%data%',work.jobs[i].employer)+HTMLworkTitle.replace('%data%',work.jobs[0].title));
+workExperience.append(HTMLworkDates.replace('%data%',work.jobs[i].dates));
+workExperience.append(HTMLworkLocation.replace('%data%',work.jobs[i].location));
+workExperience.append(HTMLworkDescription.replace('%data%',work.jobs[i].description));
+}
+
+
+
+/*Projects Json*/
+var projects = {
+  "projects": [
+    {
+      "title": "Java Developer",
+      "dates": "22-06-2015 till now",
+      "description": "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will",
+      "images": "http://s20.postimg.org/to38h51xp/world.jpg"
+    },
+    {
+      "title": "Java Developer",
+      "dates": "08-09-2008 to 22-06-2015",
+      "description": "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will",
+      "images": "http://s20.postimg.org/tzp5916a5/macbook.jpg"
+    }
+  ],
+  "display": "function(){}"
+}
+
+//Grab the projects div
+var projectsId = $("#projects");
+
+
+//Iterate the projects json
+for(i in projects.projects){
+projectsId.append(HTMLprojectStart);
+projectsId.append(HTMLprojectTitle.replace('%data%',projects.projects[i].title));
+projectsId.append(HTMLprojectDates.replace('%data%',projects.projects[i].dates));
+projectsId.append(HTMLprojectDescription.replace('%data%',projects.projects[i].description));
+projectsId.append(HTMLprojectImage.replace('%data%',projects.projects[i].images));
+}
+
+/*Education Json*/
+var education = {
+  "schools": [
+    {
+      "name": "Thiagarajar College of Engineering",
+      "location": "Madurai,India",
+      "degree": "Bachelors in Mechatronics",
+      "dates": "08-08-2004 to 08-04-2008"
+    },
+    {
+      "name": "TVSLMHSS",
+      "location": "Madurai,India",
+      "degree": "Higher Secondary",
+      "dates": "06-06-1990 to 06-06-2004"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Intro to HTML and CSS",
+      "school": "Udacity",
+      "dates": "20-08-2015",
+      "url": "http://www.udacity.com"
+    },
+    {
+      "title": "Responsive Images",
+      "school": "Udacity",
+      "dates": "17-09-2015",
+      "url": "http://www.udacity.com"
+    }
+  ]
+}
+
+//Grab the education Header
+
+var educationId = $('#education');
+
+
+//var HTMLonlineClasses = '<h3>Online Classes</h3>';
+//var HTMLonlineTitle = '<a href="#">%data%';
+//var HTMLonlineSchool = ' - %data%</a>';
+//var HTMLonlineDates = '<div class="date-text">%data%</div>';
+//var HTMLonlineURL = '<br><a href="#">%data%</a>';
+
+
+//Iterate the education schools json
+for(i in education.schools){
+
+educationId.append(HTMLschoolStart);
+educationId.append(HTMLschoolName.replace('%data%',education.schools[i].name));
+educationId.append(HTMLschoolDegree.replace('%data%',education.schools[i].degree));
+educationId.append(HTMLschoolDates.replace('%data%',education.schools[i].dates));
+educationId.append(HTMLschoolLocation.replace('%data%',education.schools[i].location));
+
+}
+educationId.append(HTMLonlineClasses);
+for(i in education.onlineCourses){
+educationId.append(HTMLschoolStart);
+educationId.append(HTMLonlineTitle.replace('%data%',education.onlineCourses[i].title));
+educationId.append(HTMLonlineSchool.replace('%data%',education.onlineCourses[i].school));
+educationId.append(HTMLonlineDates.replace('%data%',education.onlineCourses[i].dates));
+educationId.append(HTMLonlineURL.replace('%data%',education.onlineCourses[i].url));
+ 
+}
+
+
+
+
+
+
+
+
+$('#mapDiv').append(googleMap);
+
+
+
+
+
+
+
+
+
+
